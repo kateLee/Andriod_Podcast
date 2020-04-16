06a80511d43b051f87130d88ed85f23c253f5d5d
kateLee <xoolwater@gmail.com>
2020-04-16 15:11:51 +0800

    Add ProgressBar for Main and CastDetail

--- app/src/main/java/com/katelee/podcast/model/CastDetailViewModel.kt
+++ app/src/main/java/com/katelee/podcast/model/CastDetailViewModel.kt
@@ -11,15 +11,19 @@ import com.katelee.podcast.network.Repository
 class CastDetailViewModel: BaseViewModel() {
     private val repository = Repository()
     val castDetail: MutableLiveData<CastDetail> = MutableLiveData()
+    var isLoading: MutableLiveData<Boolean> = MutableLiveData()
 
     fun fetchCastDetail() = request (
         onError = {
             // handle error
 //            todo alert
+            isLoading.value = false
             Log.e("@v@CastDetailViewModel", it.message)
         },
         execute = {
+            isLoading.postValue( true)
             castDetail.postValue(repository.getCastDetail().data?.collection)
+            isLoading.postValue( false)
         }
     )
 }
--- app/src/main/java/com/katelee/podcast/model/MainViewModel.kt
+++ app/src/main/java/com/katelee/podcast/model/MainViewModel.kt
@@ -11,15 +11,19 @@ import com.katelee.podcast.network.Repository
 class MainViewModel: BaseViewModel() {
     private val repository = Repository()
     val castList: MutableLiveData<ArrayList<Cast>> = MutableLiveData()
+    var isLoading: MutableLiveData<Boolean> = MutableLiveData()
 
     fun fetchCasts() = request (
         onError = {
             // handle error
 //            todo alert
+            isLoading.value = false
             Log.e("@v@MainViewModel", it.message)
         },
         execute = {
+            isLoading.postValue( true)
             castList.postValue(repository.getCasts().data?.podcast)
+            isLoading.postValue( false)
         }
     )
 }
